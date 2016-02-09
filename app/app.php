fe004df09b568ae06525dbd81757ce06175282a2
Ronando <ronando.long@gmail.com>
2016-02-09 12:46:23 -0800

    Update app.php

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
 // require_once - It opens the autoload.php file which Composer added into the vendor folder.
+// This file is responsible for automatically loading all the other Silex files in the vendor folder into our project. Any time that you need to include external files in your code, you'll use the require_once keyword, followed by the path to the file. You need to include the path in double quotes and add a semicolon at the end of the line.
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/jobOpening.php";
 
