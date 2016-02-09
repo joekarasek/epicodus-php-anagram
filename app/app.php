6da309d2770c89af09d442a85710a2096cfcd5a0
Marika Allely and Ronando Long <student@epicodus.com>
2016-02-09 11:48:07 -0800

    edit .gitignore

--- app/app.php
+++ app/app.php
@@ -2,7 +2,13 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/jobOpening.php";
 
+    $app = new Silex\Application();
 
+    $app->get("/", function() {
+        return "Home";
+    });
+
+    return $app;
 
 
 
