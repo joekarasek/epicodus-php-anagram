1ca8f61a7a221054271705c7082409df36b2cdd1
Alex Fallenstedt and Joe Karasek <a.fallenstedt@gmail.com>
2016-02-16 09:46:42 -0800

    add method softcompare to anagram

--- src/Anagram.php
+++ src/Anagram.php
@@ -6,5 +6,39 @@
         {
             return str_split($word);
         }
+
+        // explodes two strings and compares characters.
+        function compareWords($word, $word2)
+        {
+            $explode_one = $this->explodeWord($word);
+            $explode_two = $this->explodeWord($word2);
+            sort($explode_one);//returns true if successfully sorted
+            sort($explode_two);//returns true if successfully sorted
+
+            if ($explode_one == $explode_two) {
+                return true;
+            }
+            return false;
+        }
+
+        // soft compare words
+        function softCompareWords($master, $test)
+        {
+            $master_exploded = $this->explodeWord($master);
+            $test_exploded = $this->explodeWord($test);
+
+            $match_goal = count($test_exploded);
+            $matches = 0;
+            foreach ($test_exploded as $test_character) {
+                foreach ($master_exploded as $key => $master_character) {
+                    if ($test_character == $master_character) {
+                        $matches++;
+                        unset($master_exploded[$key]);
+                        break;
+                    }
+                }
+            }
+            return $matches == $match_goal;
+        }
     }
  ?>
--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -16,5 +16,42 @@
             // assert
             $this->assertEquals(array('h','e','l','l','o'), $result);
         }
+
+        function test_Anagram_compare_words()
+        {
+            //arrange
+            $test_Anagram = new Anagram;
+            $input = 'bread';
+            $input2 = 'beard';
+            $input3 = 'zoo';
+            $input4 = 'berd';
+
+            //act
+            $result = $test_Anagram->compareWords($input, $input2);
+            $result2 = $test_Anagram->compareWords($input3, $input4);
+
+            //assert
+            $this->assertEquals(true, $result);
+            $this->assertEquals(false, $result2);
+
+        }
+
+        function test_Anagram_softcompare_words()
+        {
+            //arrange
+            $test_Anagram = new Anagram;
+            $input = "scratches";
+            $input2 = "sas";
+            $input3 = "asses";
+
+            //act
+            $result = $test_Anagram->softCompareWords($input, $input2);
+            $result2 = $test_Anagram->softCompareWords($input, $input3);
+
+            //assert
+            $this->assertEquals(true, $result);
+            $this->assertEquals(false, $result2);
+        }
+
     }
  ?>
